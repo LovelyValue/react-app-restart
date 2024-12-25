96e8ca061a03106856afd8371a2d50ad80f74c81
Lovely.Value <aid.l.v.esd@gmail.com>
2024-12-25 20:02:48 +0300

    Composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
 import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
@@ -18,16 +19,20 @@ function App() {
 	return (
 		<>
 			<Button />
-			<JournalItem
-				title={data[0].title}
-				date={data[0].date}
-				text={data[0].text}
-			/>
-			<JournalItem
-				title={data[1].title}
-				date={data[1].date}
-				text={data[1].text}
-			/>
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					date={data[0].date}
+					text={data[0].text}
+				/>
+			</CardButton>
+			<CardButton>
+				<JournalItem
+					title={data[1].title}
+					date={data[1].date}
+					text={data[1].text}
+				/>
+			</CardButton>
 		</>
 	);
 }
