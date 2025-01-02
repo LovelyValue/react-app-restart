6ae4bca34e7ec3622b9030413b308bacfe0a3e7c
Lovely.Value <aid.l.v.esd@gmail.com>
2025-01-02 16:46:31 +0300

    submit form

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,20 +1,7 @@
-import { useState } from 'react';
 import './Button.css';
 
-function Button() {
-	// let text = 'Сохранить';
-	const [text, setText] = useState('Сохранить');
-	console.log('Rerender');
-
-	const clicked = () => {
-		setText(t => t + '!');
-		console.log(text);
-	};
-	return (
-		<button onClick={clicked} className='button accent'>
-			{text}
-		</button>
-	);
+function Button({ text }) {
+	return <button className='button accent'>{text}</button>;
 }
 
 export default Button;
