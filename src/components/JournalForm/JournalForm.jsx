a7f7e65b1b36ef0dec8d0bf448d6f81aac27e0db
Lovely.Value <aid.l.v.esd@gmail.com>
2025-01-05 16:27:38 +0300

    features of useState

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -10,8 +10,22 @@ function JournalForm() {
 		console.log(inputData);
 	};
 
+	// const [state, setState] = useState({
+	// 	age: 40,
+	// });
+
+	// const [state2, setState2] = useState([1, 2, 3]);
+
 	const addJournalItem = e => {
 		e.preventDefault();
+
+		// state.age = state.age + 1;
+		// setState({ ...state });
+
+		// state2.push(5);
+
+		// setState2([...state2]);
+
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
 		console.log(formProps);
@@ -19,6 +33,8 @@ function JournalForm() {
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
+			{/* {state.age}
+			{state2} */}
 			<input type='text' name='title' />
 			<input type='date' name='date' />
 			<input type='text' name='tag' value={inputData} onChange={inputChange} />
