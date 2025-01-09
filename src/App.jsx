8abc47b2fa55f816fd16fca74e07fa1fb2c05be1
Lovely.Value <aid.l.v.esd@gmail.com>
2025-01-09 15:52:46 +0300

    ReactDevTools

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
 const INITIAL_DATA = [
-	{ id: 1, title: 'Приключение', date: new Date(), text: 'Какой-то текст' },
-	{ id: 2, title: 'Приключение', date: new Date(), text: 'Какой-то текст' },
+	// { id: 1, title: 'Приключение', date: new Date(), text: 'Какой-то текст' },
+	// { id: 2, title: 'Приключение', date: new Date(), text: 'Какой-то текст' },
 ];
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
 				text: item.text,
 				title: item.title,
 				date: new Date(item.date),
-				id: Math.max(...oldItems.map(i => i.id)) + 1,
+				id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1,
 			},
 		]);
 	};
