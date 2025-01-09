08f44d82b7e2a4531d773da3c1285cd3b98e5742
Lovely.Value <aid.l.v.esd@gmail.com>
2025-01-09 17:43:50 +0300

    changing styles

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 		setItems(oldItems => [
 			...oldItems,
 			{
-				text: item.text,
+				text: item.post,
 				title: item.title,
 				date: new Date(item.date),
 				id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1,
