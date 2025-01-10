4657fb43815ef1b6e689dcd43f7350dea7162bcd
Lovely.Value <aid.l.v.esd@gmail.com>
2025-01-10 16:54:39 +0300

    CSS modules

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Button from '../Button/Button';
-import './JournalForm.css';
+import styles from './JournalForm.module.css';
 
 function JournalForm({ onSubmit }) {
 	const [formValidState, setFormValidState] = useState({
@@ -46,16 +46,20 @@ function JournalForm({ onSubmit }) {
 	};
 
 	return (
-		<form className='journal-form' onSubmit={addJournalItem}>
+		<form className={styles['journal-form']} onSubmit={addJournalItem}>
 			<input
 				type='text'
 				name='title'
-				className={`input ${formValidState.title ? '' : 'invalid'}`}
+				className={`${styles['input']} ${
+					formValidState.title ? '' : styles['invalid']
+				}`}
 			/>
 			<input
 				type='date'
 				name='date'
-				className={`input ${formValidState.date ? '' : 'invalid'}`}
+				className={`${styles['input']} ${
+					formValidState.date ? '' : styles['invalid']
+				}`}
 			/>
 			<input type='text' name='tag' />
 			<textarea
@@ -63,7 +67,9 @@ function JournalForm({ onSubmit }) {
 				id=''
 				cols={30}
 				rows={10}
-				className={`input ${formValidState.post ? '' : 'invalid'}`}
+				className={`${styles['input']} ${
+					formValidState.post ? '' : styles['invalid']
+				}`}
 			></textarea>
 			<Button text='Сохранить' />
 		</form>
