36868a716e116ab70dd1d8b4824ec9a30ddf45e5
Lovely.Value <aid.l.v.esd@gmail.com>
2025-01-13 19:13:53 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
@@ -7,13 +7,20 @@ import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
-const INITIAL_DATA = [
-	// { id: 1, title: 'Приключение', date: new Date(), text: 'Какой-то текст' },
-	// { id: 2, title: 'Приключение', date: new Date(), text: 'Какой-то текст' },
-];
+// const INITIAL_DATA = [
+// 	// { "id": 1, "title": "Приключение", "date": "2024/03/03", "text": "Какой-то текст" },
+// 	// { id: 2, title: 'Приключение', date: new Date(), text: 'Какой-то текст' },
+// ];
 
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('data'));
+		if (data) {
+			setItems(data.map(item => ({ ...item, date: new Date(item.date) })));
+		}
+	}, []);
 
 	const addItem = item => {
 		setItems(oldItems => [
