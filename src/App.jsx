4dabf71a23718f8e385ff31a1a2e2c0678d6066e
Lovely.Value <aid.l.v.esd@gmail.com>
2025-01-14 15:54:22 +0300

    Effect dependence

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,20 @@ function App() {
 		}
 	}, []);
 
+	useEffect(() => {
+		if (items.length) {
+			console.log('Запись!');
+			localStorage.setItem('data', JSON.stringify(items));
+		}
+	}, [items]);
+
 	const addItem = item => {
 		setItems(oldItems => [
 			...oldItems,
 			{
-				text: item.post,
+				post: item.post,
 				title: item.title,
+				tag: item.tag,
 				date: new Date(item.date),
 				id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1,
 			},
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -18,7 +18,7 @@ function JournalList({ items }) {
 		<>
 			{items.sort(sortItems).map(el => (
 				<CardButton key={el.id}>
-					<JournalItem title={el.title} date={el.date} text={el.text} />
+					<JournalItem title={el.title} date={el.date} text={el.tag} />
 				</CardButton>
 			))}
 		</>
